524d8f66cb22c84a5f1a8f8005419634d83af420
Leon35-fc <leon.f35@alice.it>
2025-11-11 17:06:15 +0100

    Update artist.js

--- scripts/artist.js
+++ scripts/artist.js
@@ -1,8 +1,17 @@
 //URL dell'endpoint
-const URL = 'https://striveschool-api.herokuapp.com/api/deezer/artist/yellowcard'
+const URL = 'https://striveschool-api.herokuapp.com/api/deezer/artist/'
+
+//cerco un artista a CASO
+const randomArt = function (){
+   let artist = Math.ceil(Math.random()*100)
+   return artist
+}
+
+console.log(randomArt())
+
 //Recupera le informazioni dall'endpoint
-const getArtist= function (){
-    fetch(URL)
+const getArtist= function (id){
+    fetch(URL + randomArt())
     .then((res) => {
         if(res.ok){
             return res.json()
@@ -12,8 +21,11 @@ const getArtist= function (){
     })
     .then((artistDetails) => {
         console.log('Dettagli artista|', artistDetails)
-
+        //Sostituisce l'immagine nell'header con quella dell'artista
         topRow = document.getElementById('artist-img').src = artistDetails.picture_xl
+
+        const songs = document.querySelector('div.container.mt-4')
+        console.log(songs)
     })
     .catch((err) => {
         console.log('Errore recupero dei dati', err)
