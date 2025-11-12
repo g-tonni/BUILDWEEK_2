0d4ac91c1cad13277d801c6f8db653d2fafab2ce
Filippo-Timo <filtimo2002@gmail.com>
2025-11-12 14:40:19 +0100

    Updates

--- scripts/home.js
+++ scripts/home.js
@@ -1,63 +1,63 @@
-const albumIDs = [75621062, 302127, 119606, 121157, 103248, 302139]
+const albumIDs = [75621062, 302127, 119606, 121157, 103248, 302139];
 
 const loadBuonaseraAlbums = function () {
-  const cards = document.querySelectorAll(".buonasera-cards")
+  const cards = document.querySelectorAll(".buonasera-cards");
 
   cards.forEach((card, i) => {
-    const img = card.querySelector("img")
-    const title = card.querySelector("span")
-    const link = card.querySelector("a")
+    const img = card.querySelector("img");
+    const title = card.querySelector("span");
+    const link = card.querySelector("a");
 
     fetch(
       `https://striveschool-api.herokuapp.com/api/deezer/album/${albumIDs[i]}`
     )
       .then((res) => {
         if (res.ok) {
-          return res.json()
+          return res.json();
         } else {
-          throw new Error(res.status)
+          throw new Error(res.status);
         }
       })
       .then((album) => {
-        img.src = album.cover_medium
-        title.innerText = album.title
-        link.setAttribute("href", `albums.html?id=${albumIDs[i]}`)
+        img.src = album.cover_medium;
+        title.innerText = album.title;
+        link.setAttribute("href", `albums.html?id=${albumIDs[i]}`);
       })
-      .catch((err) => console.log("Errore API:", err))
-  })
-}
+      .catch((err) => console.log("Errore API:", err));
+  });
+};
 
-loadBuonaseraAlbums()
+loadBuonaseraAlbums();
 
-const artistID = [412, 13, 2296, 3312, 6050984, 145]
+const artistID = [412, 13, 2296, 3312, 6050984, 145];
 
 const loadAltroArtists = function () {
-  const cards = document.querySelectorAll(".altro-cards")
+  const cards = document.querySelectorAll(".altro-cards");
 
   cards.forEach((card, i) => {
-    const img = card.querySelector("img")
-    const title = card.querySelector("h6")
-    const description = card.querySelector("p")
-    const link = card.querySelector("a")
+    const img = card.querySelector("img");
+    const title = card.querySelector("h6");
+    const description = card.querySelector("p");
+    const link = card.querySelector("a");
 
     fetch(
       `https://striveschool-api.herokuapp.com/api/deezer/artist/${artistID[i]}`
     )
       .then((res) => {
         if (res.ok) {
-          return res.json()
+          return res.json();
         } else {
-          throw new Error(res.status)
+          throw new Error(res.status);
         }
       })
       .then((artist) => {
-        img.src = artist.picture_medium
-        title.innerText = artist.name
-        description.innerText = "Le migliori HIT"
-        link.setAttribute("href", `artist.html?id=${artistID[i]}`)
+        img.src = artist.picture_medium;
+        title.innerText = artist.name;
+        description.innerText = "Le migliori HIT";
+        link.setAttribute("href", `artist.html?id=${artistID[i]}`);
       })
-      .catch((err) => console.log("Errore API:", err))
-  })
-}
+      .catch((err) => console.log("Errore API:", err));
+  });
+};
 
-loadAltroArtists()
+loadAltroArtists();
