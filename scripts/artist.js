b6a5bbe7acf983d4240da0a745449c2ddd1d289b
Giada <tonni.giada.210802@gmail.com>
2025-11-13 10:46:54 +0100

    LOCAL STOAGE AUDIO

--- scripts/artist.js
+++ scripts/artist.js
@@ -44,9 +44,10 @@ const getArtist = function () {
 
       //Sostituisco il numero dei fan nell'elemento partendo da id='headers'
       const fans = header.querySelector('p')
-   
+
       //Inserisco nel HTML
-      fans.innerText = artistDetails.nb_fan.toLocaleString('it-IT') + ' ascoltatori mensili'
+      fans.innerText =
+        artistDetails.nb_fan.toLocaleString('it-IT') + ' ascoltatori mensili'
 
       //Recupero una lista di canzoni dell'artista
 
@@ -70,13 +71,15 @@ const getArtist = function () {
                 _albumImage,
                 _nListeners,
                 _songLength,
-                _preview
+                _preview,
+                _artist
               ) {
                 this.songName = _songName
                 this.albumImage = _albumImage
                 this.nListeners = _nListeners
                 this.songLength = _songLength
                 this.preview = _preview
+                this.artist = _artist
               }
             }
             console.log('SONG', bestSongs)
@@ -99,6 +102,7 @@ const getArtist = function () {
               const albumImage = bestSongs.data[i].album.cover_medium
               const nListeners = bestSongs.data[i].rank.toLocaleString('it-IT')
               const preview = bestSongs.data[i].preview
+              const artist = bestSongs.data[i].artist.name
               const songLength = duration
 
               //SERVONO SOLO PER DEBUG
@@ -125,24 +129,24 @@ const getArtist = function () {
               songInfo.classList.add('row')
               songInfo.classList.add('align-items-baseline')
               songInfo.innerHTML = `
-                                    <div class="col col-8 pe-0">
-                                        <div class="d-flex hstack gap-3 mb-2">
-                                        <p class="m-0">${i + 1}</p>
-                                        <img src="${albumImage}" alt="" width="50" id=albumImage${i}>
-                                        <button class="bottoni-album border-0" onclick="playAudio('${preview}', event)"><p class="m-0" id="songTitle${i}">${songName}</p></button>
-                                        </div>
-                                    </div>
-                                    <div class="col col-2">  
-                                        <div class="gap-3 mb-2">
-                                        <p class="">${nListeners}</p>
-                                        </div>
-                                    </div>
-                                    <div class="col col-2">  
-                                        <div class="gap-3 mb-2">
-                                        <p id="songLength${i}">${songLength}</p>
-                                        </div>
-                                    </div>
-                                    `
+                    <div class="col col-8 pe-0">
+                        <div class="d-flex hstack gap-3 mb-2">
+                        <p class="m-0">${i + 1}</p>
+                        <img src="${albumImage}" alt="" width="50" id=albumImage${i}>
+                        <button class="bottoni-album border-0" onclick="playAudio('${preview}', '${artist}', '${albumImage}', '${songName}', event)"><p class="m-0" id="songTitle${i}">${songName}</p></button>
+                        </div>
+                    </div>
+                    <div class="col col-2">  
+                        <div class="gap-3 mb-2">
+                        <p class="">${nListeners}</p>
+                        </div>
+                    </div>
+                    <div class="col col-2">  
+                        <div class="gap-3 mb-2">
+                        <p id="songLength${i}">${songLength}</p>
+                        </div>
+                    </div>
+                    `
               songs.appendChild(songInfo)
             }
 
@@ -165,39 +169,52 @@ getArtist()
 
 //PLAYER FUNZIONANTE
 
+let keyURL = 'urlAudio'
+let keyCurrenTime = 'currentTimeAudio'
+let keyTimeDuration = 'durationAudio'
+let keyImg = 'imgAudio'
+let keyTitle = 'titleAudio'
+let keyArtist = 'artistAudio'
+
 let audio
 
 const barraProgresso = document.getElementById('barra-progresso')
 const vinile = document.getElementById('img-vinile')
 
-const playAudio = function (url, e) {
+const playAudio = function (url, artist, img, title) {
   const audioURL = url
 
-//SOSTITUISCO LA DURATA DEL BRANO 
+  //SOSTITUISCO LA DURATA DEL BRANO
   //Recupero l'id del button che ha scatenato l'evento,
-  //lo converto in array e lo filtro per recuperare 
-  //solo il numero finale e lo uso per ottenere 
+  //lo converto in array e lo filtro per recuperare
+  //solo il numero finale e lo uso per ottenere
   //l'id della durata del brano.
   //Recupero solo il testo della durata del brano
   //e lo sostituisco nella durata della progressbar
 
-const songTitleId = e.target.id
-const onlyNumber = songTitleId.split('').filter(c => !isNaN(c)).join('')
-const onlyIdNumber = "songLength" + onlyNumber
-const timeValue = document.getElementById(`${onlyIdNumber}`)
-document.querySelector('footer>div:nth-of-type(2) p:nth-of-type(2)').innerText = timeValue.innerText
+  /* const songTitleId = e.target.id
+  const onlyNumber = songTitleId
+    .split('')
+    .filter((c) => !isNaN(c))
+    .join('')
+  const onlyIdNumber = 'songLength' + onlyNumber
+  const timeValue = document.getElementById(`${onlyIdNumber}`)
+  document.querySelector(
+    'footer>div:nth-of-type(2) p:nth-of-type(2)'
+  ).innerText = timeValue.innerText */
 
-//SOSTITUISCO IL TITOLO DELLA CANZONE, IL NOME DELL'ARTISTA E L'IMMAGINE
+  //SOSTITUISCO IL TITOLO DELLA CANZONE, IL NOME DELL'ARTISTA E L'IMMAGINE
   //Recupero l'id della traccia e la cover dell'album
-const albumCoverId = "albumImage" + onlyNumber
+  //const albumCoverId = 'albumImage' + onlyNumber
 
-document.getElementById('footerSongName').innerText = e.target.innerText    //Sostituisce il nome della canzone
-document.getElementById('footerArtistName').innerText = document.querySelector('h2').innerText    //Sostituisce il nome artista
-document.getElementById('footer-album-desktop').src = document.getElementById(albumCoverId).src  //Sostituisce l'immagine dell'album
-document.getElementById('footer-title-mobile').innerText = e.target.innerText + " by " + document.querySelector('h2').innerText   //Sostituisce il titolo da mobile
-console.log(e.target.id)
-  console.log("ALBUM COVER ID",albumCoverId);
-  
+  document.getElementById('footerSongName').innerText = title //Sostituisce il nome della canzone
+  document.getElementById('footerArtistName').innerText = artist //Sostituisce il nome artista
+  document.getElementById('footer-album-desktop').src = img //Sostituisce l'immagine dell'album
+  document.getElementById('footer-title-mobile').innerText =
+    title + ' by ' + artist //Sostituisce il titolo da mobile
+  /* console.log(e.target.id)
+  console.log('ALBUM COVER ID', albumCoverId) */
+
   audio = new Audio(audioURL)
 
   const playButton = document.getElementById('play-album')
@@ -206,6 +223,11 @@ console.log(e.target.id)
   const pauseButtonMobile = document.getElementById('pause-mobile')
   // console.log(playButton)
   // console.log(audio)
+
+  localStorage.setItem(keyArtist, artist)
+  localStorage.setItem(keyTitle, title)
+  localStorage.setItem(keyImg, img)
+  localStorage.setItem(keyURL, url)
 
   audio.play()
 
@@ -221,6 +243,7 @@ console.log(e.target.id)
   playButton.appendChild(audio)
 
   audio.addEventListener('canplaythrough', (event) => {
+    localStorage.setItem(keyTimeDuration, audio.duration)
     /* console.log('DURATA', audio.duration)
      */
     audio.ontimeupdate = function () {
@@ -264,8 +287,11 @@ const barraProgressoFunz = function (aud) {
     (aud.currentTime / aud.duration) * 100
   )}%`
 
+  localStorage.setItem(`keyCurrenTime`, aud.currentTime)
+
   const elapsedTime = document.querySelector('footer>div:nth-of-type(2) p.m-0')
   elapsedSec = Math.floor(aud.currentTime % 60)
-  elapsedTime.innerText = elapsedSec > 9 ? ("0:" + elapsedSec) : ("0:0" + elapsedSec) 
+  elapsedTime.innerText =
+    elapsedSec > 9 ? '0:' + elapsedSec : '0:0' + elapsedSec
   // console.log('TEMPO TRASCORSO', elapsedTime.innerText)
 }
